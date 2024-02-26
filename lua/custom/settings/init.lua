1848ca38c222ff47fcef1b67c42c3c4e0f53b2fa
Patrick Toner <patricktoner149@gmail.com>
2024-02-26 01:06:58 +0100

    Created a new custom settings file, and set the relative line numbers setting

---
+++ lua/custom/settings/init.lua
@@ -0,0 +1,7 @@
+-- Start of my custom settings for nvim --
+
+-- I want to have relative numbers so I can at a glance use motions effectively --
+vim.wo.relativenumber = true
+
+-- Return nothing, as this file should be just for doing global settings --
+return {}
